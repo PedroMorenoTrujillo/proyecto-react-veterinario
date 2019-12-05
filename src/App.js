8121b6d63fa9f1d99461ae8ff9deff8dc76f7ee0
PedroMorenoTrujillo <43439808+PedroMorenoTrujillo@users.noreply.github.com>
2019-12-05 14:03:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,36 @@ class App extends Component {
   state = {
     citas: []
   };
+  //Cuando la aplicacion ha cargado
+  componentDidMount() {
+    const citasLS = localStorage.getItem("citas");
+    if (citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      });
+    }
+  }
+
+  //cuando eliminamos o agregamos nueva cita
+  componentDidUpdate() {
+    localStorage.setItem("citas", JSON.stringify(this.state.citas));
+  }
 
   crearNuevaCita = datos => {
     //Copiar el state actual
     const citas = [...this.state.citas, datos];
     //agregar el nuevo state
     this.setState({ citas: citas });
+  };
+
+  eliminarCita = id => {
+    //hacer copia del state
+    const citasActuales = [...this.state.citas];
+    //utilizar el filter para sacar el elemendo id del array
+    const citas = citasActuales.filter(cita => cita.id !== id);
+
+    //actualizar el state
+    this.setState({ citas });
   };
   render() {
     return (
@@ -24,7 +48,10 @@ class App extends Component {
             <NuevaCita crearNuevaCita={this.crearNuevaCita} />
           </div>
           <div className="mt-5 col-md-10 mx-auto">
-            <ListaCitas />
+            <ListaCitas
+              citas={this.state.citas}
+              eliminarCita={this.eliminarCita}
+            />
           </div>
         </div>
       </div>
