f7e30a9ac020d2b015efdd56e93613931ad9cb99
PedroMorenoTrujillo <43439808+PedroMorenoTrujillo@users.noreply.github.com>
2019-12-05 14:04:04 +0100

    Update ListaCitas.js

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Cita from "./Cita";
 
-const ListaCitas = () => <h1>Desde lista citas</h1>;
+const ListaCitas = ({ citas, eliminarCita }) => {
+  //Imprimir un mensaje en base a si hay citas o no
+  const mensaje =
+    Object.keys(citas).length === 0
+      ? "No hay citas"
+      : "Administra las citas aqui";
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">{mensaje}</h2>
+        <div className="lista-citas">
+          {citas.map(cita => (
+            <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ListaCitas;
